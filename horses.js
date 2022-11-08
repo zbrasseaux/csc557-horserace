45a61852dbf0438a649e46cd794f6030fea88fff
Zach Brasseaux <zbrasseaux97@gmail.com>
2022-11-08 13:49:52 -0600

    pre-constructed horse objects

---
+++ horses.js
@@ -0,0 +1,170 @@
+module.exports = [
+	{
+		name:"Orb",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 1
+	},{
+		name:"Electric Newton",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 2
+	},{
+		name:"Fig Tree Fire",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 3
+	},{
+		name:"Old Domino",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 4
+	},{
+		name:"Abandoned Pizza Hut",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 5
+	},{
+		name:"Django Django Django",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 6
+	},{
+		name:"Green Clown",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 7
+	},{
+		name:"Dining Room Table",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 8
+	},{
+		name:"Icecream Sundae",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 9
+	},{
+		name:"Chili Dog",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 10
+	},{
+		name:"Day Old Coffe Grounds",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 11
+	},{
+		name:"First Day of Spring",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 12
+	},{
+		name:"Fried Chicken Nightmare",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 13
+	},{
+		name:"Here's Johnny",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 14
+	},{
+		name:"Saturday Morning Cartoons",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 15
+	},{
+		name:"Brand New Backpack",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 16
+	},{
+		name:"Open Secret",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 17
+	},{
+		name:"Plastic Bag",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 18
+	},{
+		name:"Courtroom Jester",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 19
+	},{
+		name:"Steve McQueen's Good Knee",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 20
+	},{
+		name:"Little Sally Walker",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 21
+	},{
+		name:"Jolly Blue Giant",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 22
+	},{
+		name:"Hopscotch",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 23
+	},{
+		name:"Garlic Parmesean",
+		avg_speed: 25,
+		color:"brown",
+		speed_range: 5,
+		race_position:0,
+		endurance_factor: 24
+}];
